8722e1ae020e2025ec3f26afab2fa3086fe340b0
WaltonG <waltongoga@gmail.com>
2022-06-11 23:37:12 +0300

    Add importing controller

---
+++ src/main/java/org/openrefine/extensions/sparql/utils/SPARQLImportingController.java
@@ -0,0 +1,92 @@
+package org.openrefine.extensions.sparql.utils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.refine.RefineServlet;
+import com.google.refine.commands.HttpUtilities;
+import com.google.refine.importing.ImportingController;
+import com.google.refine.util.JSONUtilities;
+import com.google.refine.util.ParsingUtilities;
+
+
+public class SPARQLImportingController implements ImportingController {
+
+    private static final Logger logger = LoggerFactory.getLogger("CommonsImportingController");
+    protected RefineServlet servlet;
+    public static int DEFAULT_PREVIEW_LIMIT = 50;
+    public static int DEFAULT_PROJECT_LIMIT = 0;
+
+    @Override
+    public void init(RefineServlet servlet) {
+        this.servlet = servlet;
+
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpUtilities.respond(response, "error", "GET not implemented");
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (logger.isDebugEnabled()) {
+            logger.debug("doPost Query String::{}", request.getQueryString());
+        }
+        response.setCharacterEncoding("UTF-8");
+        Properties parameters = ParsingUtilities.parseUrlParameters(request);
+
+        String subCommand = parameters.getProperty("subCommand");
+
+        if (logger.isDebugEnabled()) {
+            logger.info("doPost::subCommand::{}", subCommand);
+        }
+
+        if ("initialize-parser-ui".equals(subCommand)) {
+            doInitializeParserUI(request, response, parameters);
+        } else {
+            HttpUtilities.respond(response, "error", "No such sub command");
+        }
+
+    }
+
+    /**
+     * 
+     * @param request
+     * @param response
+     * @param parameters
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void doInitializeParserUI(HttpServletRequest request, HttpServletResponse response, Properties parameters)
+            throws ServletException, IOException {
+        if (logger.isDebugEnabled()) {
+            logger.debug("::doInitializeParserUI::");
+        }
+
+        ObjectNode result = ParsingUtilities.mapper.createObjectNode();
+        ObjectNode options = ParsingUtilities.mapper.createObjectNode();
+        JSONUtilities.safePut(result, "status", "ok");
+        JSONUtilities.safePut(result, "options", options);
+
+        JSONUtilities.safePut(options, "skipDataLines", 0);
+        JSONUtilities.safePut(options, "storeBlankRows", true);
+        JSONUtilities.safePut(options, "storeBlankCellsAsNulls", true);
+        if (logger.isDebugEnabled()) {
+            logger.debug("doInitializeParserUI:::{}", result.toString());
+        }
+
+        HttpUtilities.respond(response, result.toString());
+
+    }
+
+}
