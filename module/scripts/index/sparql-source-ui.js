a02a996b792268d7061c6c7ef3d65e0a8e1e48da
WaltonG <waltongoga@gmail.com>
2022-07-01 09:30:46 +0300

    Added endpoint textarea

--- module/scripts/index/sparql-source-ui.js
+++ module/scripts/index/sparql-source-ui.js
@@ -10,18 +10,19 @@ Refine.SPARQLSourceUI.prototype.attachUI = function(body) {
   this._body.html(DOM.loadHTML("sparql", "scripts/index/import-from-sparql-form.html"));
   this._elmts = DOM.bind(this._body);
   
+  $('#sparql-endpoint').text($.i18n('sparql-import/endpoint-label'));
   $('#or-import-sparql').text($.i18n('sparql-import/importer-label'));
   this._elmts.queryButton.html($.i18n('sparql-buttons/query'));
   
   this._elmts.queryButton.on('click',function(evt){
 	
 	var api = new wikibase.queryService.api.Sparql();
-	var query = jQueryTrim($( "#sparql-import-textarea" ).val());
+	var query = jQueryTrim($( "#sparql-query-textarea" ).val());
 	
 	api.query( query ).done(function(){
 		var json = JSON.parse( api.getResultAsJson() );
-		$("#sparql-import-textarea").val('');
-		$("#sparql-import-textarea").val(JSON.stringify(json));
+		$("#sparql-query-textarea").val('');
+		$("#sparql-query-textarea").val(JSON.stringify(json));
 		
 		self._controller.startImportJob(self._elmts.form, $.i18n('core-index-import/uploading-pasted-data'))
 	});
